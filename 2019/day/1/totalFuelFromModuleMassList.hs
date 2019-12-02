5a234c0812fe5d4c8e280352f7c62e2fc96d7d43
gracig <gerson.graciani@emc.com>
2019-12-02 00:23:40 -0300

    Day 1 chalenge done

--- 2019/day/1/totalFuelFromModuleMassList.hs
+++ 2019/day/1/totalFuelFromModuleMassList.hs
@@ -1,12 +1,33 @@
+
 main = do
     contents <- getContents 
-    let total = totalFuel [ read x::Integer | x <- lines contents ]
-    putStrLn $ (++) "O resultado é: " $ show total
+    let modules = [ read x::Integer | x <- lines contents ] -- this precious line convert the
+                                                            -- strings from the input file into
+                                                            -- a list of integers
+        total1  = totalModuleFuel modules
+        total2  = totalRequiredFuel modules
+    putStrLn $ (++) "1) The sum of the fuel requirements is: " $ show total1
+    putStrLn $ (++) "2) The sum of the fuel requirements is: " $ show total2
 
+-- moduleFuel calculates the required fuel of a mass value
 moduleFuel::Integer->Integer
-moduleFuel x = 
-    (x `div` 3) - 2
+moduleFuel mass = (mass `div` 3) - 2
 
-totalFuel::[Integer]->Integer
-totalFuel xs = 
-    foldr ( \x acc ->  moduleFuel(x) + acc ) 0 xs
+-- calculates the totalModuleFuel using foldr
+totalModuleFuel::[Integer]->Integer
+totalModuleFuel = foldr ( \x acc ->  moduleFuel(x) + acc ) 0
+
+-- recursively calculates the total requiredFuel for a mass value
+-- using guards and where statements
+requiredFuel::Integer->Integer
+requiredFuel  mass
+    | required <= 0 = 0
+    | otherwise = required + requiredFuel required
+    where required = moduleFuel mass
+
+-- calculates total RequiredFuel using foldr
+totalRequiredFuel::[Integer]->Integer
+totalRequiredFuel= foldr ( \x acc ->  requiredFuel(x) + acc ) 0
+ 
+
+
