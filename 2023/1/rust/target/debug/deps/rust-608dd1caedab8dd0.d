40523f959ba41f020d7c35a0e2d8d0ebf10b8fd9
gracig <15052330+gracig@users.noreply.github.com>
2023-12-01 16:33:58 -0300

    Remove target folder

--- 2023/1/rust/target/debug/deps/rust-608dd1caedab8dd0.d
+++
@@ -1,9 +0,0 @@
-/home/ggraciani/code/github.com/gracig/adventofcode/2023/1/rust/target/debug/deps/librust-608dd1caedab8dd0.rmeta: src/main.rs Cargo.toml
-
-/home/ggraciani/code/github.com/gracig/adventofcode/2023/1/rust/target/debug/deps/rust-608dd1caedab8dd0.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- 2023/1/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/ggraciani/code/github.com/gracig/adventofcode/2023/1/rust/target/debug/rust: /home/ggraciani/code/github.com/gracig/adventofcode/2023/1/rust/src/main.rs
