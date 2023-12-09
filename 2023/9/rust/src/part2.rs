cb54e2874d744d9dc3829f9018054f97386c2c6f
gracig <15052330+gracig@users.noreply.github.com>
2023-12-09 09:29:31 -0300

    Add part 9

--- 2023/9/rust/src/part2.rs
+++ 2023/9/rust/src/part2.rs
@@ -10,19 +10,64 @@ pub fn process(filename: &str) -> Result<(), Box<dyn Error>> {
 
 mod data {
     #[derive(Debug)]
-    pub struct Model {}
+    pub struct Model {
+        numbers: Vec<Vec<i64>>,
+    }
 
     impl Model {
-        pub fn answer(&self) -> &'static str {
-            "Dummy"
+        pub fn answer(&self) -> i64 {
+            self.numbers
+                .iter()
+                .fold(0, |acc, sequence| acc + self.extrapolate(sequence))
+        }
+
+        pub fn differences(&self, ns: &[i64]) -> Option<Vec<i64>> {
+            if ns.len() < 2 || ns.iter().all(|v| *v == 0) {
+                None
+            } else {
+                Some(
+                    ns.iter()
+                        .fold((vec![], None), |(mut acc, before), a| match before {
+                            Some(b) => {
+                                acc.push(a - b);
+                                (acc, Some(a))
+                            }
+                            None => (acc, Some(a)),
+                        })
+                        .0,
+                )
+            }
+        }
+
+        pub fn build_stack(&self, ns: &[i64]) -> Vec<Vec<i64>> {
+            let mut stack: Vec<Vec<i64>> = vec![ns.to_vec()];
+            while let Some(d) = self.differences(stack.last().unwrap()) {
+                stack.push(d);
+            }
+            stack
+        }
+
+        pub fn extrapolate(&self, ns: &[i64]) -> i64 {
+            let mut stack = self.build_stack(ns);
+            if stack.pop().is_some() {
+                let mut acc = 0;
+                while let Some(mut b) = stack.pop() {
+                    b.reverse();
+                    acc = b.last().unwrap() - acc
+                }
+                acc
+            } else {
+                0
+            }
         }
     }
 
     mod parser {
         use nom::{
-            character::complete::{digit1, multispace1, space1},
-            combinator::{map, map_res},
+            character::complete::{char, digit1, multispace1, space1},
+            combinator::{map, map_res, opt, recognize},
             multi::separated_list1,
+            sequence::pair,
             IResult,
         };
         use std::{
@@ -49,11 +94,17 @@ mod data {
         }
 
         fn model(input: &str) -> IResult<&str, Model> {
-            map(separated_list1(multispace1, separated_list1(space1, number)), |_a| Model {})(input)
+            map(separated_list1(multispace1, numbers), |numbers| Model {
+                numbers,
+            })(input)
         }
 
-        fn number(input: &str) -> IResult<&str, u64> {
-            map_res(digit1, str::parse::<u64>)(input)
+        fn numbers(input: &str) -> IResult<&str, Vec<i64>> {
+            separated_list1(space1, number)(input)
+        }
+
+        fn number(input: &str) -> IResult<&str, i64> {
+            map_res(recognize(pair(opt(char('-')), digit1)), str::parse::<i64>)(input)
         }
 
         #[cfg(test)]
